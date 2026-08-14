c8110dad54858eb4f351494792c0094b0d36457e
phuchau23 <phuchaulua123@gmail.com>
2026-08-14 14:53:27 +0700

    feat(WardBoundaryLookup): implement IWardBoundaryLookupService for PostGIS boundary lookups

--- src/Greenlens.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/Greenlens.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace Greenlens.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.18")
+                .HasAnnotation("ProductVersion", "9.0.19")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.HasPostgresExtension(modelBuilder, "postgis");
